27d706aaaaa5d061466f2ea0e293c9de14f5e79c
Thiago Lahass <lahassthiagofelippe@gmail.com>
2025-01-28 21:31:38 -0300

    :rocket:Update app.py to deploy

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def classify_email():
         return jsonify({"error": "Ocorreu um erro no processamento da solicitação."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
